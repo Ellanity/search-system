daa780418f8af6ab8c429218725ddf38e6161483
Eldar <eldarpoplauski111@gmail.com>
2023-10-26 22:58:25 +0300

    crawler
    __recalculateInverseFrequencyOfLexemsInServerDictionary
    __recalculateWeightOfLexemsInImages

--- microserver/crawler.py
+++ microserver/crawler.py
@@ -5,6 +5,7 @@ import os
 import re
 import codecs
 import json
+import math
 
 
 # web robot to index files
@@ -29,9 +30,13 @@ class WebСrawler:
         self.__documents_urls_to_delete = []
         
         # server dict
+        
+# NEED TO REWORK IT, SAVE IN JSON IN COMMON DATA
         self.__written_in_dictionary = []
+        self.__deleted_from_dictionary = []
         
         # system
+        self.__server_dictionary_common_info_path = ""
         self.__last_start = ""
         self.__working_directory = ""
         
@@ -85,10 +90,15 @@ class WebСrawler:
                     json.dump({}, server_dictionary_part_file)
         
         # common info file
-        common_info_path = os.path.join(self.__working_directory, SERVER_DICTIONARY_DIRECTORY_URL, "common_info.json")        
-        if not os.path.exists(common_info_path):
-            with open(common_info_path, "w", encoding="utf-8") as server_dictionary_common_info_file:
-                json.dump({"count_of_words": 0}, server_dictionary_common_info_file)
+        self.__server_dictionary_common_info_path = os.path.join(self.__working_directory, SERVER_DICTIONARY_DIRECTORY_URL, "common_info.json")        
+        
+        if not os.path.exists(self.__server_dictionary_common_info_path):
+            with open(self.__server_dictionary_common_info_path, "w", encoding="utf-8") as server_dictionary_common_info_file:
+                common_info_init_data = {
+                    "count_of_words": 0,
+                    "count_of_documents": 0,
+                }
+                json.dump(common_info_init_data, server_dictionary_common_info_file)
         
     def start(self, documets_from_db):
         self.__documets_from_db = documets_from_db
@@ -174,21 +184,52 @@ class WebСrawler:
             
         # split text by spaces and getting words with counts
         list_of_lexems = text_from_document.split(' ')
-        dict_of_lexems = {item: {"count": list_of_lexems.count(item), "weight": 0} for item in list_of_lexems}
-        dict_of_lexems.pop("")
+        list_of_lexems = [item for item in list_of_lexems if item != ""]
         
-        # print(dict_of_lexems)
-        print(f"dict_of_lexems created for {document_url}")
+        dict_of_lexems = {
+            item: {
+                "count": list_of_lexems.count(item), 
+                "weight": 0
+            } for item in list_of_lexems
+        }
+        
+        search_image_document = {
+            "count_of_words": len(list_of_lexems),
+            "dict_of_lexems": dict_of_lexems
+        }
 
         # create json and write dictionary there
-        temp_search_image_document_path = os.path.join(self.__working_directory, SEARCH_IMAGES_DOCUMENTS_DIRECTORY_URL, "temp", document_url + ".json")
-        with open(temp_search_image_document_path, "w", encoding="utf-8") as search_image_file:
-            json.dump(dict_of_lexems, search_image_file)
+        self.__setSearchImageDocument(document_url, search_image_document, temp=True)
 
     def __keepCharactersInStringWithRegex(self, input_string, reference_string):
         pattern = f"[^{reference_string.lower()}]"
         filtered_string = re.sub(pattern, "", input_string.lower())
         return filtered_string.lower()
+        
+    def __setSearchImageDocument(self, document_url, search_image_document, temp=False):
+        if temp:
+            search_image_document_path = os.path.join(self.__working_directory, SEARCH_IMAGES_DOCUMENTS_DIRECTORY_URL, "temp", document_url + ".json")
+        else:
+            search_image_document_path = os.path.join(self.__working_directory, SEARCH_IMAGES_DOCUMENTS_DIRECTORY_URL, document_url + ".json")
+            
+        with open(search_image_document_path, "w", encoding="utf-8") as search_image_file:
+            json.dump(search_image_document, search_image_file)
+        
+    def __getSearchImageDocument(self, document_url, temp=False):
+        search_image_document = {}
+        try:
+            if temp:
+                search_image_document_path = os.path.join(self.__working_directory, SEARCH_IMAGES_DOCUMENTS_DIRECTORY_URL, "temp", document_url + ".json")
+            else:
+                search_image_document_path = os.path.join(self.__working_directory, SEARCH_IMAGES_DOCUMENTS_DIRECTORY_URL, document_url + ".json")
+                
+            with open(search_image_document_path, "r", encoding="utf-8") as temp_search_image_document_file:
+                json_content = temp_search_image_document_file.read()
+                search_image_document = json.loads(json_content)
+        except Exception as ex:
+            print(f"No temp search image for {document_url} | {ex}")
+        
+        return search_image_document    
         
     # ### WORK WITH SEVER DICTIONARY ### #
     
@@ -196,38 +237,33 @@ class WebСrawler:
         for document in self.__documents_urls_to_add:
             self.__writeLexemsFromTempSearchImageInServerDictionary(document)
             
-        # for document in self.__documents_urls_to_readd:
-        #     self.__deleteLexemsFromSearchImageFromServerDictionary(document)
-        #     self.__writeLexemsFromTempSearchImageInServerDictionary(document)
+        for document in self.__documents_urls_to_readd:
+            self.__deleteLexemsFromSearchImageFromServerDictionary(document)
+            self.__writeLexemsFromTempSearchImageInServerDictionary(document)
             
-        # for document in self.__documents_urls_to_delete:
-        #     self.__deleteLexemsFromSearchImageFromServerDictionary(document)
+        for document in self.__documents_urls_to_delete:
+            self.__deleteLexemsFromSearchImageFromServerDictionary(document)
     
         self.__recalculateInverseFrequencyOfLexemsInServerDictionary()
-
-    def __writeLexemsFromTempSearchImageInServerDictionary(self, document_url):
-        # check that document not added to dict before
-        if document_url in self.__written_in_dictionary:
-            return
+        self.__recalculateWeightOfLexemsInImages()
         
-        # get document with common info
-        
-        # common info file
-        common_info = {}
-        common_info_path = os.path.join(self.__working_directory, SERVER_DICTIONARY_DIRECTORY_URL, "common_info.json")        
-        with open(common_info_path, "r", encoding="utf-8") as server_dictionary_common_info_file:
+    # server dictionary common info get
+    def __getServerDictionaryCommonInfo(self):
+        common_info = {}        
+        with open(self.__server_dictionary_common_info_path, "r", encoding="utf-8") as server_dictionary_common_info_file:
             json_content = server_dictionary_common_info_file.read()
             common_info = json.loads(json_content)
-        
-        # get document search image
-        search_image_document = ""
-        temp_search_image_document_path = os.path.join(self.__working_directory, SEARCH_IMAGES_DOCUMENTS_DIRECTORY_URL, "temp", document_url + ".json")
-        with open(temp_search_image_document_path, "r", encoding="utf-8") as temp_search_image_document_file:
-            json_content = temp_search_image_document_file.read()
-            search_image_document = json.loads(json_content)
-        
-        # load full dictionary
-        server_dictionary_parts = {} # TAKES REALLY A LOT OF RAM, but this solution faseter then other (load in query for ex)
+            
+        return common_info
+            
+    # server dictionary common info set
+    def __setServerDictionaryCommonInfo(self, common_info):   
+        with open(self.__server_dictionary_common_info_path, "w", encoding="utf-8") as common_info_file:
+            json.dump(common_info, common_info_file)
+       
+    # server dictionary get
+    def __getServerDictionaryParts(self):
+        server_dictionary_parts = {} # TAKES REALLY A LOT OF RAM, but this solution faster then other (load in query for ex)
         char_paths = [
             os.path.join(self.__working_directory, SERVER_DICTIONARY_DIRECTORY_URL, char + ".json") for char in ALLOWED_DICTIONARY
         ]
@@ -235,38 +271,147 @@ class WebСrawler:
             with open(char_path, "r", encoding="utf-8") as server_dictionary_part_file:
                 json_content = server_dictionary_part_file.read()
                 server_dictionary_parts[char_path] = json.loads(json_content)
+                
+        return server_dictionary_parts
+        
+    # server dictionary set
+    def __setServerDictionaryParts(self, server_dictionary_parts):   
+        char_paths = [
+            os.path.join(self.__working_directory, SERVER_DICTIONARY_DIRECTORY_URL, char + ".json") for char in ALLOWED_DICTIONARY
+        ]
+        for char_path in char_paths:
+            with open(char_path, "w", encoding="utf-8") as server_dictionary_part_file:
+                json.dump(server_dictionary_parts[char_path], server_dictionary_part_file)
+        
+    # write lexems in dict
+    def __writeLexemsFromTempSearchImageInServerDictionary(self, document_url):
+        # check that document not added to dict before
+        if document_url in self.__written_in_dictionary:
+            return
+                
+        # get temp document search image
+        search_image_document = self.__getSearchImageDocument(document_url, temp=True)
+        if search_image_document == {}:
+            return
+        
+        # common info file
+        common_info = self.__getServerDictionaryCommonInfo()
+        
+        # load full dictionary
+        server_dictionary_parts = self.__getServerDictionaryParts()
         
         # for every lexem in image check needed part of dictionary
-        for lexem in search_image_document.keys():
+        for lexem in search_image_document["dict_of_lexems"].keys():
             # getting part of dict by first char
             char = lexem[0]
             current_server_dictionary_part = os.path.join(self.__working_directory, SERVER_DICTIONARY_DIRECTORY_URL, char + ".json")
             # make structure for this word
+            if lexem is " " or lexem is "":
+                continue
             if lexem in server_dictionary_parts[current_server_dictionary_part]:
-                if lexem is " " or lexem is "":
-                    continue
-                server_dictionary_parts[current_server_dictionary_part][lexem]["count"] += search_image_document[lexem]["count"]
+                server_dictionary_parts[current_server_dictionary_part][lexem]["count"] += search_image_document["dict_of_lexems"][lexem]["count"]
+                server_dictionary_parts[current_server_dictionary_part][lexem]["documents_with_term"] += 1
             else:
-                server_dictionary_parts[current_server_dictionary_part][lexem] = {"count": search_image_document[lexem]["count"], "inverse_frequency": 0.0}
+                server_dictionary_parts[current_server_dictionary_part][lexem] = {
+                    "count": search_image_document["dict_of_lexems"][lexem]["count"], 
+                    "inverse_frequency": 0.0,
+                    "documents_with_term": 1
+                }
                 
-            common_info["count_of_words"] += search_image_document[lexem]["count"]
+            common_info["count_of_words"] += search_image_document["dict_of_lexems"][lexem]["count"]
         
+        common_info["count_of_documents"] += 1
+
         # save full dictionary
-        for char_path in char_paths:
-            with open(char_path, "w", encoding="utf-8") as server_dictionary_part_file:
-                json.dump(server_dictionary_parts[char_path], server_dictionary_part_file)
-                
+        self.__setServerDictionaryParts(server_dictionary_parts)
+        
         # save common info file
-        with open(common_info_path, "w", encoding="utf-8") as common_info_file:
-            json.dump(common_info, common_info_file)
-            
+        self.__setServerDictionaryCommonInfo(common_info)
         
         # mark, that file added to dictionary
         self.__written_in_dictionary.append(document_url)
         
-    # TODO
+    # delete lexems from dict
     def __deleteLexemsFromSearchImageFromServerDictionary(self, document_url):
-        temp_search_image_document_path = os.path.join(self.__working_directory, SEARCH_IMAGES_DOCUMENTS_DIRECTORY_URL, document_url)
+        # check that document not already deleted from dict before
+        if document_url in self.__deleted_from_dictionary:
+            return
+                
+        # try to get document search image
+        search_image_document = self.__getSearchImageDocument(document_url, temp=False)
+        if search_image_document == {}:
+            return
         
-    def __recalculateInverseFrequencyOfLexemsInServerDictionary(self): pass
-    
+        # common info file
+        common_info = self.__getServerDictionaryCommonInfo()
+        
+        # load full dictionary
+        server_dictionary_parts = self.__getServerDictionaryParts()
+        
+        # for every lexem in image check needed part of dictionary
+        for lexem in search_image_document["dict_of_lexems"].keys():
+            # getting part of dict by first char
+            char = lexem[0]
+            current_server_dictionary_part = os.path.join(self.__working_directory, SERVER_DICTIONARY_DIRECTORY_URL, char + ".json")
+            # make structure for this word
+            if lexem is " " or lexem is "":
+                continue
+            if lexem in server_dictionary_parts[current_server_dictionary_part]:
+                server_dictionary_parts[current_server_dictionary_part][lexem]["count"] = \
+                max(server_dictionary_parts[current_server_dictionary_part][lexem]["count"] - search_image_document["dict_of_lexems"][lexem]["count"], 0)
+                server_dictionary_parts[current_server_dictionary_part][lexem]["documents_with_term"] = \
+                max(server_dictionary_parts[current_server_dictionary_part][lexem]["documents_with_term"] - 1, 0)
+                
+            common_info["count_of_words"] = max(common_info["count_of_words"] - search_image_document["dict_of_lexems"][lexem]["count"], 0)
+
+        common_info["count_of_documents"] -= 1
+        
+        # save full dictionary
+        self.__setServerDictionaryParts(server_dictionary_parts)
+                
+        # save common info file
+        self.__setServerDictionaryCommonInfo(common_info)
+        
+        # mark, that file added to dictionary
+        self.__written_in_dictionary.append(document_url)
+        
+    def __recalculateInverseFrequencyOfLexemsInServerDictionary(self): 
+        # common info file
+        common_info = self.__getServerDictionaryCommonInfo()
+        
+        # load full dictionary
+        server_dictionary_parts = self.__getServerDictionaryParts()
+        
+        count_of_documents = common_info["count_of_documents"]
+        
+        for part in server_dictionary_parts.keys():
+            for lexem in server_dictionary_parts[part].keys():
+                if server_dictionary_parts[part][lexem]["documents_with_term"] != 0:
+                    inverse_frequency_of_lexem = math.log((count_of_documents / server_dictionary_parts[part][lexem]["documents_with_term"])) 
+                    server_dictionary_parts[part][lexem]["inverse_frequency"] = round(inverse_frequency_of_lexem, 10)
+                    
+        # save full dictionary
+        self.__setServerDictionaryParts(server_dictionary_parts)
+        
+        # save common info file
+        self.__setServerDictionaryCommonInfo(common_info)
+        
+    # https://ru.wikipedia.org/wiki/TF-IDF
+    def __recalculateWeightOfLexemsInImages(self): 
+        
+        def update_weights(document_url):
+            search_image_document = self.__getSearchImageDocument(document_url, temp=True)
+            if search_image_document == {}:
+                return
+            for lexem in search_image_document["dict_of_lexems"].keys():
+                search_image_document["count_of_words"] = max(search_image_document["count_of_words"], 1)
+                search_image_document["dict_of_lexems"][lexem]["weight"] = \
+                round((search_image_document["dict_of_lexems"][lexem]["count"] / search_image_document["count_of_words"]), 10)
+            self.__setSearchImageDocument(document_url, search_image_document, temp=True)
+        
+        for document in self.__documents_urls_to_add:
+            update_weights(document)
+                
+        for document in self.__documents_urls_to_readd:
+            update_weights(document)
+        
